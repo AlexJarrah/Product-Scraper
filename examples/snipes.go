d404c7fa2a02b8e4fe214d8680c7d0039de90339
quo0001 <alex.jarrah1@gmail.com>
2023-12-05 03:12:36 -0500

    Added Snipes example

---
+++ examples/snipes.go
@@ -0,0 +1,20 @@
+package examples
+
+import (
+	"fmt"
+
+	"github.com/AlexJarrah/Product-Scraper/internal/modules/snipes"
+)
+
+func snipes_() {
+	resp, err := snipes.FetchSnipesProducts("Dunk", "")
+	if err != nil {
+		panic(err)
+	}
+
+	for _, product := range resp {
+		fmt.Printf("%s %s\n", product.Name, product.Color)
+		fmt.Println(product.InStock)
+		fmt.Println(product.Price.USD)
+	}
+}
