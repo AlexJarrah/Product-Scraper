cfedd2183268a04427f575501113e05aabf3b0ef
quo0001 <alex.jarrah1@gmail.com>
2023-12-07 04:11:26 -0500

    Added JD Sports example

---
+++ examples/jdsports.go
@@ -0,0 +1,18 @@
+package examples
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/AlexJarrah/Product-Scraper/internal/modules/jdsports"
+)
+
+func jdsports_() {
+	resp, err := jdsports.FetchJDSportsProduct("FN6622-201", "")
+	if err != nil {
+		log.Panic(err)
+	}
+
+	fmt.Println(resp.Name)
+	fmt.Printf("$%f\n", resp.Price)
+}
