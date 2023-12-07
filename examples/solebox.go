590fc101e81973b357a80c90d724a575a56673c2
quo0001 <alex.jarrah1@gmail.com>
2023-12-07 03:47:23 -0500

    Added Solebox example

---
+++ examples/solebox.go
@@ -0,0 +1,20 @@
+package examples
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/AlexJarrah/Product-Scraper/internal/modules/solebox"
+)
+
+func solebox_() {
+	resp, err := solebox.FetchSoleboxProducts("black shoes", "")
+	if err != nil {
+		log.Panic(err)
+	}
+
+	for _, product := range resp {
+		fmt.Println(product.Name)
+		fmt.Println(product.Price)
+	}
+}
