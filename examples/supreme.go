ffe21cbb1c71283a89a7ef235e94784ecb745958
quo0001 <alex.jarrah1@gmail.com>
2023-12-07 02:25:30 -0500

    Added Supreme example

---
+++ examples/supreme.go
@@ -0,0 +1,35 @@
+package examples
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/AlexJarrah/Product-Scraper/internal/modules/supreme"
+)
+
+func supremeCollections() {
+	collections, err := supreme.FetchSupremeCollections("")
+	if err != nil {
+		log.Panic(err)
+	}
+
+	fmt.Println("Products:", collections.AllProductsCount)
+	for _, product := range collections.Products {
+		fmt.Println("Name:", product.Title)
+		fmt.Println("In Stock:", product.Available)
+		fmt.Printf("Price: $%.2f\n", (float32(product.Price) / 100))
+	}
+}
+
+func supremeSeason() {
+	szn, err := supreme.FetchSupremeSeason("fallwinter2023", "")
+	if err != nil {
+		log.Panic(err)
+	}
+
+	for _, group := range szn.Preview.Groups {
+		for _, product := range group.Products {
+			fmt.Println(product.Title)
+		}
+	}
+}
